a8fbd5a06dad0e1a28e4338aff53b108c4d6063d
CodeBuddies <dat500021@gmail.com>
2026-01-17 13:47:21 +0700

    Update Program.cs

--- caculator/Program.cs
+++ caculator/Program.cs
@@ -1,7 +1,7 @@
-﻿class Caculator
+class Calculator
 {
     public static void Main()
-    {
+    { double result = 0;
         while (true)
         {
             Console.WriteLine("Welcome to Simple Caculator");
@@ -13,42 +13,68 @@
             {
                 try
                 {
-                    double result = 0;
-                    Console.Write("Enter a: ");
-                    double a = double.Parse(Console.ReadLine());
-                    Console.Write("Enter b: ");
-                    double b = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter a char(+,-,*,/)");
-                    char c = char.Parse(Console.ReadLine());
-
-                    switch (c)
+                    Console.WriteLine("Please choose simple caculate or Advance(s/a))");
+                    string input = Console.ReadLine();
+                    if (input.Trim().ToLower() == "s")
                     {
-                        case '+':
-                            result = a + b;
-                            break;
-                        case '-':
-                            result = a - b;
-                            break;
-                        case '*':
-                            result = a * b;
-                            break;
-                        case '/':
-                            if (b == 0)
-                            {
-                                Console.WriteLine("Cannot divide by zero");
-                                continue;
-                            }
-                            result = a / b;
-                            break;
-                        default:
-                            Console.WriteLine("not valid");
-                            break;
+                        Console.Write("Enter a: ");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.Write("Enter b: ");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter a char(+,-,*,/)");
+                        char c = char.Parse(Console.ReadLine());
+
+                        switch (c)
+                        {
+                            case '+':
+                                result = a + b;
+                                break;
+                            case '-':
+                                result = a - b;
+                                break;
+                            case '*':
+                                result = a * b;
+                                break;
+                            case '/':
+                                if (b == 0)
+                                {
+                                    Console.WriteLine("Cannot divide by zero");
+                                    continue;
+                                }
+                                result = a / b;
+                                break;
+                            default:
+                                Console.WriteLine("not valid");
+                                break;
+                        }
+                        Console.WriteLine("Result: " + result);
                     }
-                    Console.WriteLine("Result: " + result);
+
+                    else if (input.Trim().ToLower() == "a")
+                    {
+                        Console.WriteLine("Enter number");
+                       double number = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter a ^,√");
+                        char c = char.Parse(Console.ReadLine());
+
+                        switch (c)
+                        {
+                            case '^':
+                                Console.Write("Enter Exponent:");
+                                int exponent = int.Parse(Console.ReadLine());
+                                result = Math.Pow(number, exponent);
+                                break;
+                            case '√':
+                                result = Math.Sqrt(number);
+                                break;
+                        }
+                        Console.WriteLine("Result:" +  result);
+                    }
+                    
                 }
-                catch
+                catch(Exception ex)
                 {
-                    Console.WriteLine("Error");
+                    Console.WriteLine(ex.Message);
                 }
             }
 
